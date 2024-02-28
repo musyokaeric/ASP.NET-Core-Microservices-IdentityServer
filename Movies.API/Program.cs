a63010a2a6711d521746ca64934cbf6e8b7392d1
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-02-28 11:55:53 +0300

    Building API Resource - Movie.API Asp.Net Web API Project
    In-Memory Entity Framework

---
+++ Movies.API/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.API.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Use In-Memory Database for Entity Framework Core
+builder.Services.AddDbContext<MoviesAPIContext>(options =>
+    options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("MoviesAPIContext") ?? throw new InvalidOperationException("Connection string 'MoviesAPIContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Seed In-Memory Database
+SeedDatabase(app);
+
+static void SeedDatabase(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var moviesContext = services.GetRequiredService<MoviesAPIContext>();
+        MoviesContextSeed.SeedAsync(moviesContext);
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
