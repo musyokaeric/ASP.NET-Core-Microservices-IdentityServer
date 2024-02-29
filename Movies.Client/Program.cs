c6e4e90ca059d4d1b43fa803cc0df2716ad72a9d
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-02-29 19:16:17 +0300

    Setup OpenId Connect Interactive Client for Movies.Client MVC web app

--- Movies.Client/Program.cs
+++ Movies.Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using Movies.Client.ApiServices;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Movies.Client.ApiServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,29 @@ builder.Services.AddControllersWithViews();
 
 // Movies API Services
 builder.Services.AddScoped<IMovieApiService, MovieApiService>();
+
+// OpenId Connect Services
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+    {
+        options.Authority = "https://localhost:6005";
+
+        options.ClientId = "movies_mvc_client";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+
+        options.SaveTokens = true;
+
+        options.GetClaimsFromUserInfoEndpoint = true;
+    });
 
 var app = builder.Build();
 
@@ -22,6 +47,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
