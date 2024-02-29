47a90dd151b35afbc4ae105e025060fdb2db9fc4
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-02-29 20:21:52 +0300

    Post Logout Redirect Uri Options

--- IdentityServer/Quickstart/Account/AccountOptions.cs
+++ IdentityServer/Quickstart/Account/AccountOptions.cs
@@ -13,7 +13,7 @@ namespace IdentityServerHost.Quickstart.UI
         public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
 
         public static bool ShowLogoutPrompt = true;
-        public static bool AutomaticRedirectAfterSignOut = false;
+        public static bool AutomaticRedirectAfterSignOut = true; // automatically redirects back to client home page
 
         public static string InvalidCredentialsErrorMessage = "Invalid username or password";
     }
