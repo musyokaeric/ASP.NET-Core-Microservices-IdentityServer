4b67145dfe52234a3445e429d4c9c410af189bfd
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-03-03 22:21:39 +0300

    Securing API Gateway with Bearer Token

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var authenticationProviderKey = "IdentityApiKey";
+
+// JWT Bearer Authentication
+builder.Services.AddAuthentication().AddJwtBearer(authenticationProviderKey, options =>
+{
+    options.Authority = "https://localhost:6005"; // IS4
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false,
+    };
+});
 
 // Inject Ocelot
 builder.Services.AddOcelot();
